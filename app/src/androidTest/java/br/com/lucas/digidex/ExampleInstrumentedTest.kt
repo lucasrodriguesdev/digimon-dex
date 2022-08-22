eff4cdc88ec3a8f5fdc64ae18aaa993381240b9a
Lucas Rodrigues <lucas.rbit@gmail.com>
2022-08-22 17:37:45 -0300

    teste instrumentado para navegacao entre dois fragmentos

--- app/src/androidTest/java/br/com/lucas/digidex/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/br/com/lucas/digidex/ExampleInstrumentedTest.kt
@@ -1,24 +1,68 @@
 package br.com.lucas.digidex
 
-import androidx.test.platform.app.InstrumentationRegistry
+import android.app.Application
+import androidx.fragment.app.testing.FragmentScenario
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.navigation.Navigation
+import androidx.navigation.testing.TestNavHostController
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import br.com.lucas.digidex.ui.DigimonList
+import junit.framework.Assert.assertEquals
+import org.junit.Before
+import org.junit.BeforeClass
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.junit.Assert.*
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+    private lateinit var navController: TestNavHostController
+    private lateinit var digimonList: FragmentScenario<DigimonList>
+
+    @Before
+    fun setup() {
+        navController = TestNavHostController(
+            ApplicationProvider.getApplicationContext()
+        )
+        digimonList = launchFragmentInContainer(themeResId = R.style.Theme_DigiDex)
+
+        digimonList.onFragment { fragment ->
+            navController.setGraph(R.navigation.navgraph)
+            Navigation.setViewNavController(fragment.requireView(), navController)
+        }
+    }
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("br.com.lucas.digidex", appContext.packageName)
+    fun navigate_to_details_nav_component() {
+
+        onView(withId(R.id.recyclerview_digimon_list))
+            .perform(
+                RecyclerViewActions
+                    .actionOnItemAtPosition<RecyclerView.ViewHolder>(2, click())
+            )
+
+        assertEquals(navController.currentDestination?.id, R.id.digimonDetail)
+    }
+
+    companion object{
+
+        @BeforeClass
+        @JvmStatic
+        fun setupClass(){
+            print("setup class")
+        }
+
+        @BeforeClass
+        @JvmStatic
+        fun tearDownClass(){
+            print("down class")
+        }
     }
 }
